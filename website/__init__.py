7763ee0e8246ce3a52275911412bbbcb819c998b
Ido Glazer <brtydse100@gmail.com>
2024-05-14 22:09:45 +0300

    reverse change

--- website/__init__.py
+++ website/__init__.py
@@ -18,10 +18,3 @@ def create_app():
         
     return app
 
-
-app = create_app()
-
-if __name__ == '__main__':
-    # run_simple('localhost', 8080, app, use_reloader=True)
-    app.run(host="0.0.0.0", port=3000)
-    # app.run(debug=True)
