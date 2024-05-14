dd84ea928d9de8813057a99ea8e6180ef5fa05b7
Ido Glazer <brtydse100@gmail.com>
2024-05-14 21:28:49 +0300

    final touches before deployment

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ app = create_app()
 
 if __name__ == '__main__':
     # run_simple('localhost', 8080, app, use_reloader=True)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+    # app.run(debug=True)
